1316400291e49f6c5d58f8e92f6352e0b4c36bd5
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2019-11-14 19:42:49 -0500

    Data types

---
+++ datatypes.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+// Data Types
+
+// string
+
+// int  int8  int16  int32  int64
+// uint uint8 uint16 uint32 uint64 uintptr
+
+// byte // alias for uint8
+
+// rune // alias for int32
+//      // represents a Unicode code point
+
+// float32 float64
+
+// complex64 complex128
+
+func main() {
+	var i int
+	var f float64
+	var b bool
+	var s string
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+}
